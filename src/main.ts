3302dce7988fd430411009209d218a5bcc8e21fd
HYK <yundori97@gmail.com>
2025-03-10 20:33:15 +0900

    design: 메인 페이지

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ const navAdv = document.getElementById("nav-burger-advanced");
 const bgnIcon = document.getElementById("nav-burger-beginner-icon");
 const itmIcon = document.getElementById("nav-burger-itermediate-icon");
 const advIcon = document.getElementById("nav-burger-advanced-icon");
+const scrollBtn = document.getElementById("scroll-top");
 
 navBgn?.addEventListener("click", () => {
   bgnIcon?.classList.toggle("-rotate-180");
@@ -15,4 +16,16 @@ navItm?.addEventListener("click", () => {
 
 navAdv?.addEventListener("click", () => {
   advIcon?.classList.toggle("-rotate-180");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    scrollBtn?.classList.remove("invisible");
+  } else {
+    scrollBtn?.classList.add("invisible");
+  }
+});
+
+scrollBtn?.addEventListener("click", () => {
+  window.scrollTo(0, 0);
 });
