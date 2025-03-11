04004e42ad914012ace0438bb33e8172b2ee1026
HYK <yundori97@gmail.com>
2025-03-11 12:10:55 +0900

    src 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,29 @@
 import { defineConfig } from "vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
+import { readdirSync } from "node:fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pageDir = resolve(__dirname, "src/page");
+const htmlFiles = readdirSync(pageDir).filter((file) => file.endsWith(".html"));
+
+const input = {
+  main: resolve(__dirname, "index.html"),
+  ...htmlFiles.reduce((entries, file) => {
+    const name = file.replace(".html", "");
+    // @ts-ignore
+    entries[name] = resolve(pageDir, file);
+    return entries;
+  }, {}),
+};
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/seoul-round/",
+  build: {
+    rollupOptions: {
+      input,
+    },
+  },
 });
